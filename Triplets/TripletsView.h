281836ffe240e93b3025ec23d83937e0b00b00e8
Brett Bukowski <brett.bukowski@gmail.com>
2014-09-07 14:24:09 -0600

    Initial Commit

---
+++ Triplets/TripletsView.h
@@ -0,0 +1,13 @@
+//
+//  TripletsView.h
+//  Triplets
+//
+//  Created by Brett Bukowski on 9/7/14.
+//  Copyright (c) 2014 Brett Bukowski. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TripletsView : ScreenSaverView
+
+@end
